5f0237ec17f745b5411673e8a0e14097408c0e40
Ahmad Mujtaba Saeed <85842373+Ahmad-Mujtaba-Saeed@users.noreply.github.com>
2025-02-28 15:41:41 +0500

    backend updates

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://equitycircle.techtrack.online','http://localhost:3000','*'],
+    'allowed_origins' => [''],
 
     'allowed_origins_patterns' => [],
 
